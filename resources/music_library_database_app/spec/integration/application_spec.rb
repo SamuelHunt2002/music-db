1746ad9473cd04b019159cdebe46db208bdfed09
Sam Hunt <huntsamuel2002@gmail.com>
2022-11-29 11:28:58 +0000

    Initial commit

--- resources/music_library_database_app/spec/integration/application_spec.rb
+++ resources/music_library_database_app/spec/integration/application_spec.rb
@@ -10,4 +10,19 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  it "Gets the albums" do
+    new_response = get("/albums")
+    expect(new_response.status).to eq 200
+    expect(new_response.body).to include("Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring")
+  end
+
+  it "Posts a new album" do
+    response = post("/albums", title: "Voyage", release_year: "2022", artist_id: "2")
+    expect(response.status).to eq 200
+    expect(response.body).to eq ''
+
+    new_response = get("/albums")
+    expect(new_response.status).to eq 200
+    expect(new_response.body).to include("Voyage")
+  end
 end
